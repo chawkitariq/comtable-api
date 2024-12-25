a523a75f15e387ff4f5ec59a3ab6891e31fed2fb
chawkitariq <chawkitariq@icloud.com>
2024-12-25 11:42:47 +0100

    feat(module): setup database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      entities: [],
+      synchronize: process.env.APP_ENV !== 'production',
+    }),
+  ],
+})
+export class DatabaseModule {}
