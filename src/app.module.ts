ef25e18bf82003857b775cd8d38336246ec6e283
chawkitariq <chawkitariq@icloud.com>
2024-12-25 11:46:34 +0100

    feat(module): setup tax module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { SettingModule } from './setting/setting.module';
+import { TaxModule } from './tax/tax.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, SettingModule],
+  imports: [DatabaseModule, UserModule, SettingModule, TaxModule],
   controllers: [AppController],
   providers: [AppService],
 })
