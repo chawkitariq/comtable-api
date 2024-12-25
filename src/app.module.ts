dd0eccb8c4bb659d22d69e8b2979deb3cee2797e
chawkitariq <chawkitariq@icloud.com>
2024-12-25 11:49:03 +0100

    feat(module): setup notification module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { SettingModule } from './setting/setting.module';
 import { TaxModule } from './tax/tax.module';
 import { CompanyModule } from './company/company.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, SettingModule, TaxModule, CompanyModule],
+  imports: [DatabaseModule, UserModule, SettingModule, TaxModule, CompanyModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
