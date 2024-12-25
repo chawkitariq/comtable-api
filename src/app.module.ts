ea5a904565a9d3020402bfc053d3a204c20aa7ac
chawkitariq <chawkitariq@icloud.com>
2024-12-25 11:45:33 +0100

    feat(module): setup setting module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { SettingModule } from './setting/setting.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, SettingModule],
   controllers: [AppController],
   providers: [AppService],
 })
