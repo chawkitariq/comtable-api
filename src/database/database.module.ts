41ae78014d820ce5fd5c8d614e61f0c2549c3ac3
chawkitariq <chawkitariq@icloud.com>
2024-12-27 15:23:12 +0100

    refactor: clean code

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import AppEnv from 'src/app.util';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       entities: [],
-      synchronize: process.env.APP_ENV !== 'production',
+      synchronize: !AppEnv.isProduction,
     }),
   ],
 })
