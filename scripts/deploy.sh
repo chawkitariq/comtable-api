ba0fa5d4febe6149a428359b1f8e32f6ab0f24f9
chawkitariq <chawkitariq@icloud.com>
2025-05-02 08:29:34 +0200

    ci(script): rollback to initial conf

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,27 +3,23 @@ set -e
 
 cd
 
-# Login to GitHub Container Registry on EC2
+echo "Login to GitHub Container Registry on EC2"
 echo "$GITHUB_TOKEN" | docker login $REGISTRY -u "$GITHUB_ACTOR" --password-stdin
 
-echo "Stop and remove the old container if it exists"
-
-docker stop comtable-api-container || true
-docker rm comtable-api-container || true
-  
-echo "Clean up old images"
-
-docker system prune -fa
-
 echo "Pull the latest image"
-
 docker pull $FULL_IMAGE_NAME
 
-echo "Run the new container"
+echo "Stop and remove the old container if it exists"
+docker stop comtable-api-container || true
+docker rm comtable-api-container || true
 
+echo "Run the new container"
 docker run -d --env-file ./.env --name comtable-api-container \
   -p 80:3000 \
   --restart unless-stopped \
   $FULL_IMAGE_NAME
+  
+echo "Clean up old images"
+docker system prune -f
 
 echo "Deployment completed successfully!"
